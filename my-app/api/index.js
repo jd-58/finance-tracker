bf156efa667458046588eaffde054620bcf28c0e
jd-58 <jacobdeaton58@gmail.com>
2024-07-30 16:33:18 -0400

    Tested that server running on port 4040 works.

--- my-app/api/index.js
+++ my-app/api/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+console.log('starting server');
 
-app.get('/api/test', (req, res) => {
-    res.json('test ok')
+
+app.get('/api/test', (req, res) =>
+{ console.log('Received request on /api/test');
+    res.json('test ok');
 });
 
-app.listen(4000);
+app.listen(4040, () => { console.log('Server is running on port 4040'); });
